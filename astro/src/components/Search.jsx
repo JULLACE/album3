ead545f6217c3556cf8fb49e7205d28099939073
Julio <88823380+JULLACE@users.noreply.github.com>
2025-10-04 19:11:42 -0400

    Successfully migrated React components (all of album3) to Astro

---
+++ astro/src/components/Search.jsx
@@ -0,0 +1,13 @@
+const Search = ({ value, handleChange, submitHandler }) => {
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        value={value}
+        onChange={handleChange}
+        placeholder="Search for an Album..."
+        enterKeyHint="search" />
+    </form>
+  );
+};
+
+export default Search;
